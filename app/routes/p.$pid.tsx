0c7b77781d07563e031953d9e20920148ac123fe
Lody Borgers <l.borgers@taf.nl>
2023-04-21 14:48:06 +0200

    Cherry-picked page preview fix to this branch

--- app/routes/p.$pid.tsx
+++ app/routes/p.$pid.tsx
@@ -1,27 +1,92 @@
 import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useCatch } from '@remix-run/react';
 import invariant from 'tiny-invariant';
 import type { PostOrPage } from 'tryghost__content-api';
 
-import { adminPosts, getPostByUUID } from '~/models/posts.server';
+import { getAdminPages, getAdminPosts } from '~/models/posts.server';
 import PostContent from '~/components/PostContent';
+
 export const loader = async ({ params }: LoaderArgs) => {
   const { pid } = params;
 
-  const list = await adminPosts();
+  const posts = getAdminPosts();
+  const pages = getAdminPages();
+  const results = await Promise.allSettled([posts, pages]);
+
+  invariant(results, 'No posts or pages found');
+
+  const list: PostOrPage[] = results.flatMap((result) => {
+    if (result.status === 'fulfilled') {
+      return result.value;
+    }
+
+    return [];
+  });
 
   invariant(pid, 'Post UUID is required');
 
   const previewPost = list.find((item: PostOrPage) => item.uuid === pid);
 
-  const post = await getPostByUUID(previewPost.uuid, previewPost.slug);
+  if (!previewPost || !previewPost.uuid) {
+    throw json(
+      {
+        message: 'No post or pages found.',
+        description: `Uuid ${pid} returns no post or pages. Maybe check the post/pages list to see what we actually have.`,
+        list,
+      },
+      { status: 404 },
+    );
+  }
 
-  return json({ post });
+  return json({ post: previewPost });
 };
 
 export default function PostPreview() {
-  const { post } = useLoaderData<typeof loader>();
+  const { post } = useLoaderData();
 
-  return <PostContent post={post} />;
+  return (
+    <section className="px-5 md:px-10 xl:px-40 mx-auto flex flex-col">
+      <PostContent post={post} />
+    </section>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  console.trace(error.stack);
+
+  return (
+    <section className="px-5 md:px-10 xl:px-40 mx-auto flex flex-col mt-10">
+      <h1 className="text-4xl">{error.message}</h1>
+    </section>
+  );
+}
+
+export function CatchBoundary() {
+  const { data } = useCatch();
+
+  // console.dir(data.list);
+  return (
+    <section className="px-5 md:px-10 xl:px-40 mx-auto flex flex-col mt-10">
+      <div
+        className="mt-10 prose
+        prose-sm
+        sm:prose-base
+        md:prose-lg
+        xl:prose-2xl
+        prose-nord
+        dark:prose-invert
+        prose-a:no-underline
+        prose-a:border-b-2
+        prose-a:pb-1
+        prose-a:border-b-nord-frost-1-400
+        prose-a:transition-all
+        hover:prose-a:border-b-nord-frost-1-600"
+      >
+        <h1>{data.message}</h1>
+        <p>{data.description}</p>
+      </div>
+    </section>
+  );
 }
